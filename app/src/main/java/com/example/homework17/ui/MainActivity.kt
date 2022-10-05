f0369925b09a7e49045645155c4a92fe868acc06
manjaro <dimitri.jibuti1123@gmail.com>
2022-10-05 08:53:36 +0400

    initial

--- app/src/main/java/com/example/homework17/ui/MainActivity.kt
+++ app/src/main/java/com/example/homework17/ui/MainActivity.kt
@@ -2,13 +2,21 @@ package com.example.homework17.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import com.example.homework17.R
 import com.example.homework17.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var toggle: ActionBarDrawerToggle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        toggle = ActionBarDrawerToggle(this, binding.root, R.string.open, R.string.close)
+        binding.root.addDrawerListener(toggle)
+        toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
     }
 }
